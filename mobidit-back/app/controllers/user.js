2bc0d3c519649628704e10b7b4a138dcea103ee2
Anthropohpobe <anthropophobe@MacBook-Air-de-Anthropohpobe.local>
2023-06-26 15:30:55 +0200

    adding update user

--- mobidit-back/app/controllers/user.js
+++ mobidit-back/app/controllers/user.js
@@ -57,32 +57,33 @@ const create = async (req, res) => {
 	}
 };
 
-// const update = async (req, res) => {
-// 	const {
-// 		params: { userId },
-// 	} = req;
-// 	const { body } = req;
-// 	try {
-// 		const updateUser = await prisma.users.update({
-// 			where: {
-// 				id: parseInt(userId, 10),
-// 			},
-// 			data: {
-// 				...body,
-// 			},
-// 		});
-// 		return res.json({
-// 			succes: true,
-// 			data: updateUser,
-// 			code: 200,
-// 		});
-// 	} catch (error) {
-// 		return res.json({ succes: false, data: { error } });
-// 	}
-// };
+const update = async (req, res) => {
+	const {
+		params: { userId },
+	} = req;
+	const { body } = req;
+	try {
+		const updateUser = await prisma.users.update({
+			where: {
+				id: parseInt(userId, 10),
+			},
+			data: {
+				...body,
+			},
+		});
+		return res.json({
+			succes: true,
+			data: updateUser,
+			code: 200,
+		});
+	} catch (error) {
+		return res.json({ succes: false, data: { error } });
+	}
+};
 
 module.exports = {
 	index,
 	show,
 	create,
+	update
 };
--- mobidit-back/app/routes/user.js
+++ mobidit-back/app/routes/user.js
@@ -6,6 +6,6 @@ const UserController = require('../controllers/user');
 router.get('/', UserController.index);
 router.get('/:userId', UserController.show);
 router.post('/create', UserController.create);
-// router.put('/:userId/update', UserController.update);
+router.put('/:userId/update', UserController.update);
 
 module.exports = router;
