971d7104402eae7e4cbee30db0217daf85d0193a
Anthropohpobe <anthropophobe@MacBook-Air-de-Anthropohpobe.local>
2023-06-27 12:18:14 +0200

    adding CRUD post controler

--- mobidit-back/app/routes/index.js
+++ mobidit-back/app/routes/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 //IMPORT SUB ROUTES
 const users = require('./user');
-// const posts = require('./posts');
+const posts = require('./post');
 
 
 router.use('/users', users);
-// router.use('/posts', posts);
+router.use('/posts', posts);
 
 
 module.exports = router;
---
+++ mobidit-back/app/routes/post.js
@@ -0,0 +1,12 @@
+const router = require('express').Router({
+	mergeParams: true,
+});
+const UserController = require('../controllers/post');
+
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.post('/create', UserController.create);
+router.put('/:id/update', UserController.update);
+router.delete('/:id/delete', UserController.supprimer);
+
+module.exports = router;
