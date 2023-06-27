c6f644003faca5e181069dd030b52151b980f6db
Ayoub-Moutaouakil <73231595+Ayoub-Moutaouakil@users.noreply.github.com>
2023-06-27 13:57:45 +0200

    Composant d'inscription

--- mobidit-front/src/App.jsx
+++ mobidit-front/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Feed from './components/Feed'
+import Register from './components/Register'
 
 function App() {
   return (
-    <Feed></Feed>
+    <Register></Register>
   )
 }
 
-export default App
+export default App;
